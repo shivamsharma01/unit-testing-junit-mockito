47ccee6d752a765d5a6983112a72bcf3a13aeafe
Shivam Sharma <prisonmatch@gmail.com>
2020-02-27 15:10:57 +0530

    verify - atLeast, atMost, atLeastOnce, never, times

--- src/test/java/com/in28minutes/unittesting/unittesting/service/ListMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/service/ListMockTest.java
@@ -3,32 +3,62 @@ package com.in28minutes.unittesting.unittesting.service;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 public class ListMockTest {
-	List mock =mock(List.class);
-	
+	List<String> mock = mock(List.class);
+
 	@Test
 	public void size_basic() {
 		when(mock.size()).thenReturn(5);
 		assertEquals(5, mock.size());
 	}
-	
+
 	@Test
 	public void returnDifferentValues() {
 		when(mock.size()).thenReturn(5).thenReturn(10);
 		assertEquals(5, mock.size());
 		assertEquals(10, mock.size());
-		assertEquals(10, mock.size());
 	}
-	
+
 	@Test
 	public void returnWithParameters() {
 		when(mock.get(0)).thenReturn("in28Minutes");
 		assertEquals("in28Minutes", mock.get(0));
 		assertEquals(null, mock.get(1));
 	}
+
+	@Test
+	public void returnWithGenericParameters() {
+		when(mock.get(anyInt())).thenReturn("in28Minutes");
+		assertEquals("in28Minutes", mock.get(0));
+		assertEquals("in28Minutes", mock.get(1));
+	}
+
+	// check internal method invocations by the class we are testing
+	@Test
+	public void verificationBasics() {
+		String value1 = mock.get(0);
+		String value2 = mock.get(1);
+		
+		verify(mock).get(0);
+		verify(mock, times(2)).get(anyInt());
+		verify(mock, atLeast(1)).get(anyInt());
+					//or
+		verify(mock, atLeastOnce()).get(anyInt());
+		
+		verify(mock, atMost(2)).get(anyInt());
+		verify(mock, never()).get(2);
+	}
+
 }
